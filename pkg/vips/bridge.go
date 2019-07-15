ee4c88197682ecb996582802187ba4e2ca319cfe
elad laufer <elad@wix.com>
2019-07-15 14:02:00 +0300

    - unref

--- pkg/vips/bridge.go
+++ pkg/vips/bridge.go
@@ -67,7 +67,7 @@ func vipsCallOperation(operation *C.VipsOperation, options []*Option) error {
 		return handleVipsError()
 	}
 
-	defer unref(unsafe.Pointer(operation))
+	defer unrefPointer(unsafe.Pointer(operation))
 
 	// Write back the outputs
 	for _, option := range options {
@@ -175,7 +175,7 @@ func vipsExportBuffer(image *C.VipsImage, params *ExportParams) ([]byte, ImageTy
 	// If these are equal, then we don't want to deref the original image as
 	// the original will be returned if the target colorspace is not supported
 	if tmpImage != image {
-		defer unref(tmpImage)()
+		defer unrefImage(tmpImage)()
 	}
 
 	cLen := C.size_t(0)
@@ -278,7 +278,7 @@ func vipsFlattenBackground(input *C.VipsImage, color Color) (*C.VipsImage, error
 		if int(err) != 0 {
 			return nil, handleVipsError()
 		}
-		unref(input)()
+		unrefImage(input)()
 
 		input = output
 	}
@@ -294,7 +294,7 @@ func vipsResize(input *C.VipsImage, scale, vscale float64, kernel Kernel) (*C.Vi
 	scale = math.Min(scale, maxScaleFactor)
 	vscale = math.Min(vscale, maxScaleFactor)
 
-	defer unref(input)()
+	defer unrefImage(input)()
 
 	if err := C.resize_image(input, &output, C.double(scale), C.double(vscale), C.int(kernel)); err != 0 {
 		return nil, handleVipsError()
@@ -307,7 +307,7 @@ func vipsRotate(input *C.VipsImage, angle Angle) (*C.VipsImage, error) {
 	incOpCounter("rot")
 	var output *C.VipsImage
 
-	defer unref(input)()
+	defer unrefImage(input)()
 
 	if err := C.rot_image(input, &output, C.VipsAngle(angle)); err != 0 {
 		return nil, handleVipsError()
@@ -324,7 +324,7 @@ func vipsComposite(inputs []*C.VipsImage, mode BlendMode) (*C.VipsImage, error) 
 		return nil, handleVipsError()
 	}
 
-	// todo: why no unref?
+	// todo: why no unrefImage?
 
 	return output, nil
 }
@@ -337,7 +337,7 @@ func vipsAddAlpha(input *C.VipsImage) (*C.VipsImage, error) {
 	incOpCounter("addAlpha")
 	var output *C.VipsImage
 
-	defer unref(input)()
+	defer unrefImage(input)()
 
 	if err := C.add_alpha(input, &output); err != 0 {
 		return nil, handleVipsError()
@@ -350,8 +350,8 @@ func vipsAdd(left *C.VipsImage, right *C.VipsImage) (*C.VipsImage, error) {
 	incOpCounter("add")
 	var output *C.VipsImage
 
-	defer unref(left)()
-	defer unref(right)()
+	defer unrefImage(left)()
+	defer unrefImage(right)()
 
 	if err := C.add(left, right, &output); err != 0 {
 		return nil, handleVipsError()
@@ -364,8 +364,8 @@ func vipsMultiply(left *C.VipsImage, right *C.VipsImage) (*C.VipsImage, error) {
 	incOpCounter("multiply")
 	var output *C.VipsImage
 
-	defer unref(left)()
-	defer unref(right)()
+	defer unrefImage(left)()
+	defer unrefImage(right)()
 
 	if err := C.multiply(left, right, &output); err != 0 {
 		return nil, handleVipsError()
@@ -378,7 +378,7 @@ func vipsExtractBand(input *C.VipsImage, band, num int) (*C.VipsImage, error) {
 	incOpCounter("extractBand")
 	var output *C.VipsImage
 
-	defer unref(input)()
+	defer unrefImage(input)()
 
 	if err := C.extract_band(input, &output, C.int(band), C.int(num)); err != 0 {
 		return nil, handleVipsError()
@@ -393,7 +393,7 @@ func vipsBandJoin(inputs []*C.VipsImage) (*C.VipsImage, error) {
 
 	defer func() {
 		for _, image := range inputs {
-			unref(image)()
+			unrefImage(image)()
 		}
 	}()
 
@@ -408,7 +408,7 @@ func vipsLinear1(input *C.VipsImage, a, b float64) (*C.VipsImage, error) {
 	incOpCounter("linear1")
 	var output *C.VipsImage
 
-	defer unref(input)()
+	defer unrefImage(input)()
 
 	if err := C.linear1(input, &output, C.double(a), C.double(b)); err != 0 {
 		return nil, handleVipsError()
@@ -421,7 +421,7 @@ func vipsExtractArea(input *C.VipsImage, left, top, width, height int) (*C.VipsI
 	incOpCounter("extractArea")
 	var output *C.VipsImage
 
-	defer unref(input)()
+	defer unrefImage(input)()
 
 	if err := C.extract_image_area(input, &output, C.int(left), C.int(top), C.int(width), C.int(height)); err != 0 {
 		return nil, handleVipsError()
@@ -434,7 +434,7 @@ func vipsEmbed(input *C.VipsImage, left, top, width, height int, extend Extend) 
 	incOpCounter("embed")
 	var output *C.VipsImage
 
-	defer unref(input)()
+	defer unrefImage(input)()
 
 	if err := C.embed_image(input, &output, C.int(left), C.int(top), C.int(width), C.int(height), C.int(extend), 0, 0, 0); err != 0 {
 		return nil, handleVipsError()
@@ -447,7 +447,7 @@ func vipsFlip(input *C.VipsImage, dir Direction) (*C.VipsImage, error) {
 	incOpCounter("flip")
 	var output *C.VipsImage
 
-	defer unref(input)()
+	defer unrefImage(input)()
 
 	if err := C.flip_image(input, &output, C.int(dir)); err != 0 {
 		return nil, handleVipsError()
@@ -460,7 +460,7 @@ func vipsInvert(input *C.VipsImage) (*C.VipsImage, error) {
 	incOpCounter("invert")
 	var output *C.VipsImage
 
-	defer unref(input)()
+	defer unrefImage(input)()
 
 	if err := C.invert_image(input, &output); err != 0 {
 		return nil, handleVipsError()
@@ -473,7 +473,7 @@ func vipsGaussianBlur(input *C.VipsImage, sigma float64) (*C.VipsImage, error) {
 	incOpCounter("gaussblur")
 	var output *C.VipsImage
 
-	defer unref(input)()
+	defer unrefImage(input)()
 
 	if err := C.gaussian_blur(input, &output, C.double(sigma)); err != 0 {
 		return nil, handleVipsError()
@@ -486,7 +486,7 @@ func vipsZoom(input *C.VipsImage, xFactor, yFactor int) (*C.VipsImage, error) {
 	incOpCounter("zoom")
 	var output *C.VipsImage
 
-	defer unref(input)()
+	defer unrefImage(input)()
 
 	if err := C.zoom_image(input, &output, C.int(xFactor), C.int(yFactor)); err != 0 {
 		return nil, handleVipsError()
@@ -499,7 +499,7 @@ func vipsLabel(input *C.VipsImage, lp LabelParams) (*C.VipsImage, error) {
 	incOpCounter("label")
 	var output *C.VipsImage
 
-	defer unref(input)()
+	defer unrefImage(input)()
 
 	text := C.CString(lp.Text)
 	defer C.free(unsafe.Pointer(text))
@@ -548,7 +548,17 @@ func handleVipsError() error {
 	return fmt.Errorf("%v\nStack:\n%s", s, stack)
 }
 
-func unref(ref interface{}) func() {
+func unrefImage(ref *C.VipsImage) func() {
+	if ref == nil {
+		panic("nil ref")
+	}
+
+	return func() {
+		C.g_object_unref(C.gpointer(ref))
+	}
+}
+
+func unrefPointer(ref unsafe.Pointer) func() {
 	if ref == nil {
 		panic("nil ref")
 	}
